8421a1611776ffa0b6a5f315c6fecb56cc87d5b5
Giedre <gieal640@student.liu.se>
2023-09-01 09:59:13 +0200

    post/read and write works. convert to bootstrap:(

--- lab1/script.js
+++ lab1/script.js
@@ -19,11 +19,13 @@ window.onload = () => {
 }
 
 function create_post(text){
+    let date_time = new Date().toUTCString();
+
     let old_text = getCookie("post")
     if(old_text == false){
-        document.cookie = "post=" + text + "; expires=Thu, 31 Aug 2055 11:11:00 GMT+0200; path=/;" // Special case for first post
+        document.cookie = "post=" +  date_time + "§" + text + "; expires=Thu, 31 Aug 2055 11:11:00 GMT+0200; path=/;" // Special case for first post
     } else {
-        document.cookie = "post=" + old_text + "§" + text + "; expires=Thu, 31 Aug 2055 11:11:00 GMT+0200; path=/;"
+        document.cookie = "post=" + old_text + "§" + date_time + "§" + text + "; expires=Thu, 31 Aug 2055 11:11:00 GMT+0200; path=/;"
     }
 }
 
@@ -33,11 +35,28 @@ function display_posts(parent){
     }
     let all_posts = getCookie("post").split('§')
     all_posts = all_posts.reverse()
-    for(let i = 0; i < all_posts.length; i+=1){
+    for(let i = 0; i < all_posts.length; i+=2){
         let post = document.createElement("article")
+
         let post_content = document.createElement("p")
         post_content.appendChild(document.createTextNode(all_posts[i]))
         post.appendChild(post_content)
+
+        let post_date = document.createElement("p")
+        post_date.appendChild(document.createTextNode(all_posts[i+1]))
+        post_date.classList.add("date")
+        post.appendChild(post_date)
+
+        let img = document.createElement("img")
+        img.src = "https://en.bloggif.com/tmp/f0795844f7de69a8f4e5fe05d8fc43c3/text.gif?1693553120"
+        img.classList.add("author")
+        post.appendChild(img)
+
+        let checkbox = document.createElement("input")
+        checkbox.type  = "checkbox"
+        checkbox.classList.add("readbox")
+        post.appendChild(checkbox)
+
         parent.appendChild(post)
     }
 }
